f6bdac42095e0b3f6fec73d2146b48f1198017a2
luisfelipe-ads-maker <luisfharduim95@gmail.com>
2025-09-13 19:22:53 +0000

    criei as variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,11 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado1[10], estado2[10];
+    char codigo1[10], codigo2[10];
+    char nome1[20], nome2[20];
+    int populacao1, pontos1, populacao2, pontos2;
+    float area1, pib1, area2, pib2;
 
   // Área para entrada de dados
 
